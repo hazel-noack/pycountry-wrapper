248e12c08c096bd4bf016af1d886ba60d85c69d1
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-06-12 14:37:38 +0200

    feat: playing with new

--- pycountry_wrapper/__main__.py
+++ pycountry_wrapper/__main__.py
@@ -1,8 +1,8 @@
 from .__about__ import __name__, __version__
-from . import Country
+from . import Country, EmptyCountry
 
 
 def cli():
     print(f"Running {__name__} version {__version__} from __main__.py")
 
-    print(Country(country="DE").name)
+    print(EmptyCountry(country="doesn't exist").name)
