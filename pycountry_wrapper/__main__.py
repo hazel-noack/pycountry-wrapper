8bf0d6d6f56fafd8630541bd19de0cc4d2ad30d8
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-06-12 14:40:23 +0200

    fix: lookup error for fuzzy search

--- pycountry_wrapper/__main__.py
+++ pycountry_wrapper/__main__.py
@@ -5,4 +5,4 @@ from . import Country, EmptyCountry
 def cli():
     print(f"Running {__name__} version {__version__} from __main__.py")
 
-    print(EmptyCountry(country="doesn't exist").name)
+    print(EmptyCountry(country="zwx").name)
