9ec253262d29ed4f817ca44a38b130d093b5a069
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-06-12 15:09:06 +0200

    fix: fallback only worked if not searching

--- pycountry_wrapper/__main__.py
+++ pycountry_wrapper/__main__.py
@@ -1,11 +1,12 @@
 from .__about__ import __name__, __version__
-from . import Country, EmptyCountry
+from . import Country, EmptyCountry, config
 
 import pycountry
 
 
 def cli():
     print(f"Running {__name__} version {__version__} from __main__.py")
+    config.fallback_country = "US"
     t = pycountry.countries.get(alpha_2="DE")
 
     country = EmptyCountry(pycountry_object=t)
